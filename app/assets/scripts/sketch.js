2211eaf8998e392618ce3ce1edb28dbca226ba01
Paul <pfreema1@gmail.com>
2018-04-16 09:39:35 -0400

    init

---
+++ app/assets/scripts/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  background("#323232");
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  background("#323232");
+
+  fill("#FCFBFC");
+  ellipse(50, 50, 80, 80);
+}
